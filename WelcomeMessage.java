bec3b1d45b7a97be57124fd1c2a2f2d6880bd3e1
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-23 18:40:55 +0530

    Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package CarInvoice;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Car Invoice Generator Program....");
+
+	}
+
+}
